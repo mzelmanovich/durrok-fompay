80f811e68e0e5bebfe47df2727079119e7d74343
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-04-24 19:39:53 -0400

    added album test

---
+++ tests/routes/albums.routes.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../../server/app');
+const server = request(app);
+
+describe('/albums', () => {
+
+  it('can find via id (albums/:id', (done) => {
+    server.get('/albums/1')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end( (body) => {
+      expect(body.id * 1).to.equal(1);
+      done();
+    });
+  });
+
+  it('returns 404', (done) => {
+    server.get('/albums/10')
+    .expect('Content-Type', /json/)
+    .expect(404)
+    .end( () => {
+      done();
+    });
+  });
+});
---
+++ tests/routes/index.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const db = require('../../server/db');
+
+describe('Routes', function() {
+  beforeEach(done => {
+    db.seed(true).then(() => done()).catch(done);
+  });
+
+  afterEach(done => {
+    db.seed(true).then(() => done()).catch(done);
+  });
+
+  describe('api', () => {
+    require('./albums.routes.test');
+  });
+});
