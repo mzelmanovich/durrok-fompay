4107a054e37392ace739ebed193832e7f739af0e
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2017-04-25 21:31:43 -0400

    mount

--- server/routes/api/songs.js
+++ server/routes/api/songs.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
 const db = require('../../db');
 
-router.get('/songs/:id', (req, res, next) => {
-  db.models.Songs.findById(req.params.id)
-  .then(song => ( song ? res.json(song) : res.sendStatus(404)))
-  .catch(next);
-});
-
 router.get('/songs/:id/artist', (req, res, next) => 
   db.models.Songs.findById(req.params.id)
   .then(song => {
@@ -15,5 +9,13 @@ router.get('/songs/:id/artist', (req, res, next) =>
   .then(artist=>( artist ? res.json(artist) : res.sendStatus(404)))
   .catch(next);
 });
+
+router.get('/songs/:id', (req, res, next) => {
+  db.models.Songs.findById(req.params.id)
+  .then(song => ( song ? res.json(song) : res.sendStatus(404)))
+  .catch(next);
+});
+
+
 
 module.exports = router;
