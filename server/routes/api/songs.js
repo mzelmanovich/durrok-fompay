41a6bba7206af70b820d51dd2ae0761bd67394b6
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2017-04-25 20:20:12 -0400

    song:/id and song:/id/artist routes

---
+++ server/routes/api/songs.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const db = require('../../db');
+
+router.get('/songs/:id', (req, res, next) => {
+  const {id} = req.params;
+  db.models.Songs.findById(id)
+  .then(song => ( song ? res.json(song) : res.sendStatus(404)))
+  .catch(next);
+});
+
+router.get('/songs/:id/artist', (req, res, next) => {
+  const {id} = req.params;
+  db.models.Songs.findById(id)
+  .then(song => {
+  	db.models.Artists.findById(song.artistId)
+  })
+  .then(artist=>( artist ? res.json(artist) : res.sendStatus(404)))
+  .catch(next);
+});
+
+module.exports = router;
