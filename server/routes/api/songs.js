6d38969bb424995a0a7033b865a359cc1f7a799b
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2017-04-25 21:53:24 -0400

    fffix

--- server/routes/api/songs.js
+++ server/routes/api/songs.js
@@ -4,6 +4,7 @@ const db = require('../../db');
 router.get('/songs/:id/artist', (req, res, next) => {
   db.models.Songs.findById(req.params.id)
   .then(song => {
+  	console.log(song)
   	db.models.Artists.findById(song.artistId)
   })
   .then(artist=>( artist ? res.json(artist) : res.sendStatus(404)))
