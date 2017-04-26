47463d570ce1ce81bc6e575aa32bfad60d8cf759
Alexander Abramov <alexanderabramov@Alexanders-MacBook-Pro-3.local>
2017-04-26 19:30:06 -0400

    artist-id-song-#61

--- server/routes/api/artists.js
+++ server/routes/api/artists.js
@@ -8,4 +8,11 @@ router.get('/artists/:id', (req, res, next) => {
   .catch(next);
 });
 
+router.get('/artists/:id/songs', (req, res, next) => {
+  const {id} = req.params;
+  db.models.Artists.findbyId(id)
+  .then(artist => ( res.json(artist.songs) : res.sendStatus(404)))
+  .catch(next);
+})
+
 module.exports = router;
