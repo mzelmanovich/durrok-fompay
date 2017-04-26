f139b0b3bc4568e85a3d21490470311a5fff72b7
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-04-25 20:27:51 -0400

    updated tests

--- tests/db/song.model.test.js
+++ tests/db/song.model.test.js
@@ -17,12 +17,12 @@ describe('Song Model', () => {
       .findAll()
       .then(results => {
         expect(results.length).to.equal(2);
-        expect(results[0].name).to.equal('Calmer');
-        expect(results[0].year).to.equal('2004');
-        expect(results[0].imgURL).to.be.null;
-        expect(results[1].duration).to.equal(244);
-        expect(results[1].price * 1).to.equal(1.99);
+        expect(results[1].name).to.equal('Calmer');
+        expect(results[1].year).to.equal('2004');
         expect(results[1].imgURL).to.be.null;
+        expect(results[0].duration).to.equal(244);
+        expect(results[0].price * 1).to.equal(1.99);
+        expect(results[0].imgURL).to.be.null;
         done();
       })
       .catch(done);
