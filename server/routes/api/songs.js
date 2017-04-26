6f9fcd1a582b4d6867b04dea7aec8b2b0727435c
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2017-04-25 21:13:28 -0400

    console

--- server/routes/api/songs.js
+++ server/routes/api/songs.js
@@ -3,13 +3,20 @@ const db = require('../../db');
 
 router.get('/song/:id', (req, res, next) => {
   db.models.Songs.findById(req.params.id)
-  .then(song => ( song ? res.json(song) : res.sendStatus(404)))
+  .then(song => {
+  	console.log(song);
+  	if(!song){
+  		res.sendStatus(404)
+  	}
+  	res.json(song)
+  })
   .catch(next);
 });
 
 router.get('/song/:id/artist', (req, res, next) => 
   db.models.Songs.findById(req.params.id)
   .then(song => {
+  	console.log(song);
   	db.models.Artists.findById(song.artistId)
   })
   .then(artist=>( artist ? res.json(artist) : res.sendStatus(404)))
