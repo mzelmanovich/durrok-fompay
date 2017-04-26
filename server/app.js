b41395dcc887f908e1528d468bfecd6d3cd95f61
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-04-25 20:52:36 -0400

    added webpack config

--- server/app.js
+++ server/app.js
@@ -8,7 +8,8 @@ app.use(bodyParser.json());
 
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
-
 app.use(routes);
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+
 module.exports = app;
