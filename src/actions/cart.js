253724b501766af34c22b3aef65813d0165da252
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-09 01:35:22 -0400

    added function

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -8,6 +8,13 @@ export const addToCart = (album) => ({
   data: album
 });
 
+export const saveOfflineCart = () => dispatch =>
+deserializeCart()
+.then(({albums}) => albums.length ?
+axios.post('/api/users/me/cart', albums)
+.then(() => dispatch(fetchCart())) : null)
+.then(console.log);
+
 
 export const putInCart = ({id}) => dispatch => {
   axios.put('/api/users/me/cart', {albumId: id})
@@ -19,4 +26,41 @@ export const removeFromCart = ({id}) => dispatch => {
   axios.delete('/api/users/me/cart', {data: {albumId: id}})
   .then(() => dispatch(fetchCart()))
   .catch(console.log);
+};
+
+
+export const serializeCart = (albums) => {
+  albums = albums.map(({id}) => id).filter(item => item > 0);
+  localStorage.setItem('cart', albums.join(','));
+};
+
+export const deserializeCart = () => {
+  let ids = localStorage.getItem('cart').split(',');
+  ids = ids.map((id) => axios.get('/api/albums/' + id).then(({data}) => data));
+  return Promise.all(ids).then((albums) => ({albums}));
+};
+
+export const addToOffLineCart = (album) => dispatch => {
+  deserializeCart().then(({albums = []}) => {
+    const index = albums.findIndex(({id}) => id === album.id);
+    if (index < 0){
+      albums.push(album);
+    }
+    albums = albums.filter(({id}) => id > -1);
+    serializeCart(albums);
+    dispatch(setCart({albums}));
+  });
+};
+
+export const removeFromOfflineCart = (album) => dispatch => {
+  deserializeCart().then(({albums = []}) => {
+    const index = albums.findIndex(({id}) => id === album.id);
+    if (index > -1){
+      albums.splice(index, 1);
+      albums = Array.from(albums);
+    }
+    serializeCart(albums);
+    dispatch(setCart({albums}));
+
+  });
 };
