56b45ba87b07250c0e01b046354194e0d78cdcd2
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-09 02:31:44 -0400

    added seed env var

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('./server/app');
 const http = require('http');
 
 const port = process.env.PORT || 3000;
+const seed = !!process.env.SEED;
 
-db.seed(true).then(() => http.createServer(app).listen(port, () => console.log(`listening on port ${port}`)));
+db.seed(seed).then(() => http.createServer(app).listen(port, () => console.log(`listening on port ${port}`)));
 
